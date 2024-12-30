f0001d302535dc5b53df9eb9d27dd1b0ab06577a
kess4obehi <kessiena4excellence@gmail.com>
2024-12-30 05:17:32 +0100

    new commit made

--- frontend/event-browsing/src/components/Footer.js
+++ frontend/event-browsing/src/components/Footer.js
@@ -5,6 +5,10 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white p-4 mt-6">
       <div className="container mx-auto text-center">
         <p>&copy; 2025 Ukphi. Connecting Communities Through Events. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a href="#" className="hover:underline">Privacy Policy</a>
+          <a href="#" className="hover:underline">Terms of Service</a>
+        </div>
       </div>
     </footer>
   );
